9491ea3e2d3fc8afc18ed803ae43d4b8c80a7613
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-04-11 19:02:57 +0700

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+
 import Palette from "./components/Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
 function App() {
   return (
-    <div className="App">
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
+    <Switch>
+      <Route
+        exact
+        path="/"
+        render={() => {
+          return <h1>PALETTE LIST GOES HERE</h1>;
+        }}
+      />
+      <Route
+        exact
+        path="/palette/:id"
+        render={() => {
+          return <h1>INDIVIDUAL PALETTE</h1>;
+        }}
+      />
+    </Switch>
+
+    // <div className="App">
+    //   <Palette palette={generatePalette(seedColors[4])} />
+    // </div>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,7 +32,7 @@ export default class Navbar extends Component {
     return (
       <header className="Navbar">
         <div className="logo">
-          <a href="#">ReactColorPicker</a>
+          <a href="/">ReactColorPicker</a>
         </div>
         <div className="slider-container">
           <span>Level: {level}</span>
