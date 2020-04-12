cbbf5f3397a026999094a54ecf490340412d0d1a
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-04-12 17:02:07 +0700

    Add Navbar and Footer to SingleColorPalette

--- src/components/Palette.jsx
+++ src/components/Palette.jsx
@@ -4,6 +4,7 @@ import ColorBox from "./ColorBox";
 import Navbar from "./Navbar";
 
 import "./Palette.css";
+import PaletteFooter from "./PaletteFooter";
 
 export default class Palette extends Component {
   constructor(props) {
@@ -44,12 +45,10 @@ export default class Palette extends Component {
           level={level}
           changeLevel={this.changeLevel}
           handleChange={this.changeFormat}
+          showingAllColors
         />
         <div className="Palette-colors">{colorBoxes}</div>
-        <footer className="Palette-footer">
-          {paletteName}
-          <span className="emoji">{emoji}</span>
-        </footer>
+        <PaletteFooter paletteName={paletteName} emoji={emoji} />
       </div>
     );
   }
