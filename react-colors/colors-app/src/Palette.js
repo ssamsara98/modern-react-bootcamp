abc50359cf1bee5cd1dfe1d0e913679dbc7fd790
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-11-04 17:46:57 +0700

    The Massive Color Project - I

---
+++ react-colors/colors-app/src/Palette.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import ColorBox from './ColorBox';
+import Navbar from './Navbar';
+import './Palette.css';
+
+class Palette extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { level: 500 };
+    this.changeLevel = this.changeLevel.bind(this);
+  }
+  changeLevel(level) {
+    this.setState({ level });
+  }
+  render() {
+    const { colors } = this.props.palette;
+    const { level } = this.state;
+    const colorBoxes = colors[level].map((color) => (
+      <ColorBox background={color.hex} name={color.name} />
+    ));
+    return (
+      <div className="Palette">
+        <Navbar level={level} changeLevel={this.changeLevel} />
+        {/* Navbar goes here */}
+        <div className="Palette-colors">{colorBoxes}</div>
+        {/* footer eventually */}
+      </div>
+    );
+  }
+}
+export default Palette;
